adbef1f4eb69835f447c998216070ee669b5ad1a
jon-ebu <ebueng23@gmail.com>
2025-06-16 14:32:09 -0700

    Updating config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  base: '/shan-tsw-770-2screen-2input',
 });
+
